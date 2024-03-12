62dc5152d04191f5c69dfba1cb715d3f552e297e
Todd Cullen <culltp1@gmail.com>
2024-03-12 15:08:43 -0700

    adding typewriter effect

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,3 +7,82 @@ menuTrigger.onclick = function() {
     menuTrigger.classList.toggle('is-active')
     body.classList.toggle('lock-scroll')
 }
+
+// Provides a typewriter effect
+const compressData = (values) => {
+    const toCompressedItem = (lastValue, item) => {
+        if (lastValue.length === item.value.length) {
+            return null;
+        }
+
+        if (lastValue === item.value.slice(0, -1)) {
+            return { op: 'a', v: item.value.slice(-1), t: item.time };
+        }
+
+        if (lastValue.length - 1 === item.value.length && lastValue.slice(0, -1) === item.value) {
+            return { op: 'd', t: item.time };
+        }
+
+        return { op: 'r', v: item.value, t: item.time };
+    }
+
+    const compressed = [];
+    let lastValue = '';
+
+    for (let i = 0; i < values.length; i++) {
+        let item = values[i];
+        let compressedItem = toCompressedItem(lastValue, item);
+
+        if (compressedItem) {
+            compressed.push(compressedItem);
+            lastValue = item.value;
+        }
+    }
+
+    return compressed;
+};
+
+const replayText = (element, values) => {
+    element.textContent = '';
+    const printingStart = new Date();
+
+    let localInterval = setInterval(() => {
+        const timePassed = new Date() - printingStart;
+
+        const value = values.shift();
+
+        if (!value) {
+            clearInterval(localInterval);
+
+            return;
+        }
+
+        if (value.t > timePassed) {
+            values.unshift(value);
+
+            return;
+        }
+
+        switch (value.op) {
+            case 'a':
+                element.textContent = element.textContent + value.v;
+                break;
+            case 'd':
+                element.textContent = element.textContent.slice(0, -1);
+                break;
+            default:
+                element.textContent = value.v;
+        }
+    }, 20);
+
+    return localInterval;
+}
+
+window.onload = () => {
+    const elements = document.querySelectorAll('h1');
+    elements.forEach((element) => {
+        const values = element.textContent;
+        const compressed = compressData(values);
+        replayText(element, compressed);
+    });
+}
